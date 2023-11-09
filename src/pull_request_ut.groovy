7a16dd3a5ffc701bd11c434debbf35fd3aa78703
gzroy <gzroy@163.com>
2023-11-09 15:20:13 +0800

    fff

--- src/pull_request_ut.groovy
+++ src/pull_request_ut.groovy
@@ -49,6 +49,13 @@ pipeline {
         }
       }
     }
+    stage("Check credentials") {
+      withCredentials([usernamePassword(credentialsId: '2b498f25-c7ea-4f67-b416-479c2f92b48f', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')])
+      steps {
+        sh 'echo $PASSWORD'
+      }
+    }
+    /*
     stage("test"){
       when {
         expression {
@@ -74,9 +81,17 @@ pipeline {
           """
         }
         success {
-          echo 'unit test successa!'
+          sh """
+          (curl -L -X POST \
+          -H \"Accept: application/vnd.github+json\" \
+          -H \"Authorization: Bearer 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\" \
+          -H \"X-GitHub-Api-Version: 2022-11-28\" \
+          ${comments_url} \
+          -d \'{\"body\": \"UT test success for commit ${sha}\"}\')
+          """
         }
       }
     }
+    */
   }
 }
